3e7a817805f1c882a4d7df96d8c5661a84dd2541
Camilo Ibanez <you@example.com>
2026-03-12 14:43:34 -0500

    Add project files.

---
+++ SportsLeague.Domain/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface IGenericRepository<T> where T : AuditBase
+
+    {
+
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T?> GetByIdAsync(int id);
+
+        Task<T> CreateAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(int id);
+
+        Task<bool> ExistsAsync(int id);
+
+    }
+}
